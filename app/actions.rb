9d3b223af7abee4581d3e82913da0ebed042ee21
Hubert Fordan <hub.fordan@gmail.com>
2017-04-06 01:16:08 +0000

    W4D2 Validations

---
+++ app/actions.rb
@@ -0,0 +1,9 @@
+get '/' do
+    @posts = Post.order(created_at: :desc)
+    erb(:index)
+end
+
+get '/signup' do        # if a user navigates to the path "/signup",
+    @user = User.new    # setup empty @user object
+    erb(:signup)        # render "app/views/signup.erb"
+end
